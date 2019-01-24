a4ffc43518de0a88e822f5e965df02bb62ad2068
Geonwoo Jeong <geonwoo.jeong@gmail.com>
2019-01-24 20:17:28 +0900

    GlobalStyles and Header

--- src/Components/App.tsx
+++ src/Components/App.tsx
@@ -1,13 +1,13 @@
 import * as React from "react";
 import Router from "Components/Router";
-import Header from "./Header";
+import GlobalStyles from "./GlobalStyles";
 
 class App extends React.Component {
   render() {
     return (
       <>
-        <Header />
         <Router />
+        <GlobalStyles />
       </>
     );
   }
