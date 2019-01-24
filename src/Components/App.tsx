9c8bd913767bd15375e80b341da71d0615860b43
Geonwoo Jeong <geonwoo.jeong@gmail.com>
2019-01-24 19:28:41 +0900

    React Router part Two

--- src/Components/App.tsx
+++ src/Components/App.tsx
@@ -1,10 +1,12 @@
 import * as React from "react";
 import Router from "Components/Router";
+import Header from "./Header";
 
 class App extends React.Component {
   render() {
     return (
       <>
+        <Header />
         <Router />
       </>
     );
