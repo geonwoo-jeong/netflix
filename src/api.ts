81ecf617d89cd35126903eec23ed1035284d9387
Geonwoo Jeong <geonwoo.jeong@gmail.com>
2019-01-25 19:04:20 +0900

    API Verbs part Two

--- src/api.ts
+++ src/api.ts
@@ -12,11 +12,35 @@ export const movieApi = {
   nowPlaying: () => api.get("movie/now_playing"),
   upcoming: () => api.get("movie/upcoming"),
   topRated: () => api.get("movie/top_rated"),
-  popular: () => api.get("movie/popular")
+  popular: () => api.get("movie/popular"),
+  movieDetail: (id: string) =>
+    api.get(`movie/${id}`, {
+      params: {
+        append_to_response: "videos"
+      }
+    }),
+  search: (term: string) =>
+    api.get("search/movie", {
+      params: {
+        query: encodeURIComponent(term)
+      }
+    })
 };
 
 export const tvApi = {
   topRated: () => api.get("tv/top_rated"),
   popluar: () => api.get("tv/popular"),
-  airingToday: () => api.get("tv/airing_today")
+  airingToday: () => api.get("tv/airing_today"),
+  showDetail: (id: string) =>
+    api.get(`tv/${id}`, {
+      params: {
+        append_to_response: "videos"
+      }
+    }),
+  search: (term: string) =>
+    api.get("search/tv", {
+      params: {
+        query: encodeURIComponent(term)
+      }
+    })
 };
