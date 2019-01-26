09b2112e95028f469dd806af489d0473ba7d564d
mlc_jeong <geonwoo.jeong@gmail.com>
2019-01-26 15:26:26 +0900

    Detail Container part One

--- src/Routes/Detail/DetailContainer.tsx
+++ src/Routes/Detail/DetailContainer.tsx
@@ -1,12 +1,28 @@
 import * as React from "react";
 import DetailPresenter from "./DetailPresenter";
+import { RouteComponentProps } from "react-router";
 
-class DetailContainer extends React.Component {
+interface IProps extends RouteComponentProps<any> {}
+class DetailContainer extends React.Component<IProps> {
   state = {
     result: null,
     error: null,
     loading: true
   };
+
+  async componentDidMount() {
+    const {
+      match: {
+        params: { id }
+      },
+      history: { push }
+    } = this.props;
+    const parsedId = parseInt(id);
+    if (isNaN(parsedId)) {
+      return push("/");
+    }
+  }
+
   render() {
     const { result, error, loading } = this.state;
     return <DetailPresenter result={result} error={error} loading={loading} />;
