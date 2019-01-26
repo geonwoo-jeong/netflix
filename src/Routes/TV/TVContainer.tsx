408014264209630f503ae16ec778964a9eb3c629
mlc_jeong <geonwoo.jeong@gmail.com>
2019-01-26 14:42:22 +0900

    TV Container

--- src/Routes/TV/TVContainer.tsx
+++ src/Routes/TV/TVContainer.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import TVPresenter from "./TVPresenter";
+import { tvApi } from "api";
 
 class TVContainer extends React.Component {
   state = {
@@ -9,8 +10,34 @@ class TVContainer extends React.Component {
     loading: true,
     error: null
   };
+
+  async componentDidMount() {
+    try {
+      const {
+        data: { results: topRated }
+      } = await tvApi.topRated();
+      const {
+        data: { results: popular }
+      } = await tvApi.popular();
+      const {
+        data: { results: airingToday }
+      } = await tvApi.airingToday();
+
+      this.setState({
+        topRated,
+        popular,
+        airingToday
+      });
+    } catch {
+      this.setState({ error: "Can't find TV Information." });
+    } finally {
+      this.setState({ loading: false });
+    }
+  }
+
   render() {
     const { topRated, popular, airingToday, loading, error } = this.state;
+    console.log(this.state);
 
     return (
       <TVPresenter
