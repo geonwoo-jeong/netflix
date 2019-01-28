1df55642e2f9ed03cd16ed39928bc34a2c292d0a
Geonwoo Jeong <geonwoo.jeong@gmail.com>
2019-01-28 19:24:10 +0900

    HomePresenter and Section Components

--- src/Routes/Home/HomePresenter.tsx
+++ src/Routes/Home/HomePresenter.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
+import styled from "styled-components";
+import Section from "Components/Section";
 
 interface IProps {
   nowPlaying: any;
@@ -9,13 +11,36 @@ interface IProps {
   loading: boolean;
 }
 
+const Container = styled.div`
+  padding: 0px 10px;
+`;
+
 const HomePresenter: React.SFC<IProps> = ({
   nowPlaying,
   upcoming,
   popular,
   error,
   loading
-}) => <div>Home</div>;
+}) =>
+  loading ? null : (
+    <Container>
+      {nowPlaying && nowPlaying.length > 0 && (
+        <Section title="Now Playing">
+          {nowPlaying.map((movie: any) => movie.title)}
+        </Section>
+      )}
+      {upcoming && upcoming.length > 0 && (
+        <Section title="Upcoming Playing">
+          {upcoming.map((movie: any) => movie.title)}
+        </Section>
+      )}
+      {popular && popular.length > 0 && (
+        <Section title="Popular Playing">
+          {popular.map((movie: any) => movie.title)}
+        </Section>
+      )}
+    </Container>
+  );
 
 HomePresenter.propTypes = {
   nowPlaying: PropTypes.array,
