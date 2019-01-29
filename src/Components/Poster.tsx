29fe646ecbfe30a25d57de6999a1306789b142d4
mlc_jeong <geonwoo.jeong@gmail.com>
2019-01-29 13:05:44 +0900

    <Poster

---
+++ src/Components/Poster.tsx
@@ -0,0 +1,67 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+interface IProps {
+  id: number;
+  imageUrl: any;
+  rating: number;
+  year: any;
+  title: string;
+  isMovie?: boolean;
+}
+
+interface ImageProps {
+  bgUrl: string;
+}
+
+const Container = styled.div``;
+
+const ImageContainer = styled.div``;
+
+const Image = styled.div`
+  background-image: url(${(props: ImageProps) => props.bgUrl});
+`;
+
+const Rating = styled.span``;
+
+const Title = styled.span``;
+
+const Year = styled.span``;
+
+const Poster: React.SFC<IProps> = ({
+  id,
+  imageUrl,
+  title,
+  rating,
+  year,
+  isMovie = false
+}) => (
+  <Link to={isMovie ? `/movie/${id}` : `/show/${id}`}>
+    <Container>
+      <ImageContainer>
+        <Image bgUrl={imageUrl} />
+        <Rating>
+          <span role="img" aria-label="rating">
+            ⭐️
+          </span>
+          {rating}/10
+        </Rating>
+      </ImageContainer>
+      <Title>{title}</Title>
+      <Year>{year}</Year>
+    </Container>
+  </Link>
+);
+
+Poster.propTypes = {
+  id: PropTypes.number.isRequired,
+  imageUrl: PropTypes.string,
+  title: PropTypes.string.isRequired,
+  rating: PropTypes.number.isRequired,
+  year: PropTypes.string,
+  isMovie: PropTypes.bool
+};
+
+export default Poster;
--- src/Routes/Home/HomePresenter.tsx
+++ src/Routes/Home/HomePresenter.tsx
@@ -4,6 +4,7 @@ import styled from "styled-components";
 import Section from "Components/Section";
 import Loader from "Components/Loader";
 import Message from "Components/Message";
+import Poster from "Components/Poster";
 
 interface IProps {
   nowPlaying: any;
@@ -31,21 +32,45 @@ const HomePresenter: React.SFC<IProps> = ({
       {nowPlaying && nowPlaying.length > 0 && (
         <Section title="Now Playing">
           {nowPlaying.map((movie: any) => (
-            <span key={movie.id}>{movie.title}</span>
+            <Poster
+              key={movie.id}
+              id={movie.id}
+              imageUrl={movie.poster_path}
+              title={movie.original_title}
+              rating={movie.vote_average}
+              year={movie.release_date.substring(0, 4)}
+              isMovie={true}
+            />
           ))}
         </Section>
       )}
       {upcoming && upcoming.length > 0 && (
         <Section title="Upcoming Playing">
           {upcoming.map((movie: any) => (
-            <span key={movie.id}>{movie.title}</span>
+            <Poster
+              key={movie.id}
+              id={movie.id}
+              rating={movie.vote_average}
+              imageUrl={movie.poster_path}
+              title={movie.original_title}
+              year={movie.release_date.substring(0, 4)}
+              isMovie={true}
+            />
           ))}
         </Section>
       )}
       {popular && popular.length > 0 && (
         <Section title="Popular Playing">
           {popular.map((movie: any) => (
-            <span key={movie.id}> {movie.title}</span>
+            <Poster
+              key={movie.id}
+              id={movie.id}
+              rating={movie.vote_average}
+              imageUrl={movie.poster_path}
+              title={movie.original_title}
+              year={movie.release_date.substring(0, 4)}
+              isMovie={true}
+            />
           ))}
         </Section>
       )}
