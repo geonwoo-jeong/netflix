15c574b901fde7d054978a95fbcab4bf2473033e
Geonwoo Jeong <geonwoo.jeong@gmail.com>
2019-01-31 19:37:40 +0900

    Updates

---
+++ precache-manifest.1d6bda67996d1944f76ecd54f73af347.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "87a0eb6a89037f2aa1b57bba7d2db7e1",
+    "url": "/netflix/static/media/noPosterSmall.87a0eb6a.png"
+  },
+  {
+    "revision": "d476b23bf7a51c955a09",
+    "url": "/netflix/static/js/runtime~main.d476b23b.js"
+  },
+  {
+    "revision": "5ed8ac07818c9b10951b",
+    "url": "/netflix/static/js/main.5ed8ac07.chunk.js"
+  },
+  {
+    "revision": "7657eb8684751427377a",
+    "url": "/netflix/static/js/1.7657eb86.chunk.js"
+  },
+  {
+    "revision": "44404018cd744621890a5c5eed95b01b",
+    "url": "/netflix/index.html"
+  }
+];
